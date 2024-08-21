2b3d102eedc7732d5f4179efa4a69a60ef3fdd01
Tabish1511 <khaqantabish@gmail.com>
2024-08-21 15:02:44 +0200

    marketing project - ready to push to siphiwo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className='text-white'>
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
         <p className='text-[#00df9a] font-bold p-2'>
-          GROWING WITH DATA ANALYTICS
+          LOREM IPSUM DOLOR SIT AMET
         </p>
         <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
           Grow with data.
@@ -23,7 +23,7 @@ const Hero = () => {
             loop
           />
         </div>
-        <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.</p>
+        <p className='md:text-2xl text-xl font-bold text-gray-500'>dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
         <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
       </div>
     </div>
